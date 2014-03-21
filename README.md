e437738f6f704e00a2a02ed7617f354689c06d06
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2014-03-21 16:56:57 +0000

    Struggling with Windows problem

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-DataHackLeiden
-==============
+Taverna Player Client for running Taverna Workflows from IPython Notebook.
+=========================================
 
-Code developed during the  NBC Data Enrichment Hackathon in Leiden 
+  Copyright (c) 2014, University of Manchester, myGrid Team, <support@mygrid.org.uk>
+  Copyright (c) 2014, Youri Lammers, Naturalis
+  Copyright (c) 2014, Ross Mounce, University of Bath
+  Distributed under the terms of the MIT License. 
+
+
+Code developed during the  NBC Data Enrichment Hackathon in Leiden
--- tavernaPlayerClient/__init__.py
+++ tavernaPlayerClient/__init__.py
@@ -1,13 +1,14 @@
 #  Taverna Player Client for running Taverna Workflows from IPython Notebook.
 #-----------------------------------------------------------------------------
-#  Copyright (c) 2014, Alan R. Wiliams myGrid Team, <support@mygrid.org.uk>
+#  Copyright (c) 2014, University of Manchester, myGrid Team, <support@mygrid.org.uk>
 #  Copyright (c) 2014, Youri Lammers, Naturalis
-#  Copyright (c) 2014, Aleksandra Pawlik, myGrid Team
+#  Copyright (c) 2014, Ross Mounce, University of Bath
 #  Distributed under the terms of the MIT License. 
 #
 #-----------------------------------------------------------------------------
     
     
-def setup():
+
+def setup(): 
     from TavernaPlayerClient import TavernaPlayerClient
     from Workflow import Workflow
